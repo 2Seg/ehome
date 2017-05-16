2055952237ee2ab1c9dca9ff2cef74e9da837082
drahcobac <dylan.pereirafcp@gmail.com>
2017-05-16 10:27:48 +0200

    Modification Dylan
    -page notre entreprise
    -page nos produits
    -ajout du footer
    -ajout du menu pour la page nos produits
    -ajout de 3 pages (capteurs, actionneurs, cameras)
    -ajout des images
    -modification de l'index (pour le menu)

--- views/about-us.php
+++ views/about-us.php
@@ -1,13 +1,68 @@
-<?php
-/*
-vue gérant l'affichage de la page "Notre entreprise"
-*/
+<html>
 
-$titre = 'Notre entreprise';
+  <head>
 
-$menu = menu($_SESSION['type']);
+  </head>
 
-$contenu = 'Notre entreprise';
+  <body>
 
-include('gabarit.php');
-?>
+    <header>
+      <?php
+      $titre = 'Notre entreprise';
+      $menu = menu($_SESSION['type']);
+      $contenu = 'Notre entreprise';
+      include('gabarit.php');
+      ?>
+    </header>
+
+    <main>
+      <h1> <strong> Notre entreprise </strong> </h1>
+
+      <h2> Notre histoire </h2>
+
+      <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+          ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+          humanitatis multiformibus officiis retentabant. Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+            ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+            humanitatis multiformibus officiis retentabant.</p>
+
+      <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+          ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+          humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+          ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+          humanitatis multiformibus officiis retentabant.</p>
+
+      <ul>
+        <li> Créée en 2016 </li>
+        <li> 92% de satisfaction</li>
+        <li> Prix de l'innovation 2016 </li>
+      </ul>
+
+      <h2> Nos objectifs </h2>
+
+      <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+          ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+          humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+            ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+            humanitatis multiformibus officiis retentabant.</p>
+
+      <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+          ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+          humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
+            ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
+            humanitatis multiformibus officiis retentabant.</p>
+
+    </main>
+
+    <p><img src="ehome/views/ressources/images/maison_entreprise.jpeg" alt="Maison Entreprise"  title="Cliquez pour agrandir" /></p>
+
+    <footer>
+      <?php
+      $footer = footer();
+      ?>
+    </footer>
+
+
+  </body>
+
+</html>
--- views/products.php
+++ views/products.php
@@ -1,13 +1,37 @@
-<?php
+
+<html>
+
+  <head>
+
+  </head>
+
+  <body>
+
+    <header>
+      <?php
 /*
 vue gérant l'affichage de la page "Nos produits"
 */
+      $titre = 'Nos produits';
+      $menu = menu($_SESSION['type']);
+      $contenu = 'Nos produits';
+      include('gabarit.php');
+      $menu_products = menu_products('')
+      ?>
+    </header>
 
-$titre = 'Nos produits';
+    <main>
 
-$menu = menu($_SESSION['type']);
+    </main>
 
-$contenu = 'Nos produits';
 
-include('gabarit.php');
-?>
+    <footer>
+      <?php
+      $footer = footer();
+      ?>
+    </footer>
+
+
+  </body>
+
+</html>
