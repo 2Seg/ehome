5a747366b1c05cfe280737cdc0f9d457a9d544aa
drahcobac <dylan.pereirafcp@gmail.com>
2017-05-18 12:16:20 +0200

    CSS #1
    - ajout de css sur structure
    - mise en place de div et classe pour l'application du css
    
    * regardez les problèmes pour le contenu (changez de place la fonction
    appellé dans $contenu (peut être mettez la dans commun.php) pour voir si
    ça change quelque chose)
    *les polices ne fonctionnent pas

--- views/home.php
+++ views/home.php
@@ -2,11 +2,14 @@
 /*
 vue gérant l'affichage de la page "Accueil"
 */
+
 $titre = 'Accueil';
 
 $menu = menu($_SESSION['type']);
 
 $contenu = 'Accueil';
+
+$footer = footer('');
 
 include('gabarit.php');
 ?>
--- views/join-us_1.php
+++ views/join-us_1.php
@@ -8,6 +8,7 @@ $menu = menu($_SESSION['type']);
 
 $contenu = '<h2>Page 1/2</h2>';
 $contenu .= form_subscribe('');
+$footer = footer();
 
 include('gabarit.php');
 ?>
