3151e65cdf37a082f681d8a543cf240733ea7867
drahcobac <dylan.pereirafcp@gmail.com>
2017-05-20 23:19:34 +0200

    CSS part 1
    - ajout d'images
    - mise en pages des articles et formulaires
    - mise en place du fond
    - contenu produits

--- views/commun.php
+++ views/commun.php
@@ -20,8 +20,11 @@ function menu() {
         } elseif ($_SESSION['type'] == 'admin') {
           echo('<li class="menu_elements"><a class="text_menu" href="index.php?cible=disconnect">SE DECONNECTER</a></li>');
           echo ('<li class="menu_elements"><a class="text_menu" href="index.php?cible=profil"><img src="views/ressources/icons/m_default_admin.png" alt="avatar"></a></li>');
-        }
-      } else {
+        } /*else {
+        echo('<li class="menu_elements"><a class="text_menu" href="index.php?cible=join-us">NOUS REJOINDRE</a></li>');
+        echo('<li class="menu_elements"><a class="text_menu" href="index.php?cible=signin">CONNEXION</a></li>');*/
+      }
+      else {
       echo('<li class="menu_elements"><a class="text_menu" href="index.php?cible=join-us">NOUS REJOINDRE</a></li>');
       echo('<li class="menu_elements"><a class="text_menu" href="index.php?cible=signin">CONNEXION</a></li>');
     }
@@ -82,6 +85,8 @@ function form_signin($erreur) {
   ?>
 
   <form method="post" action="index.php?cible=connect">
+    <section>
+    <article>
     <fieldset>
       <legend>Connexion</legend>
       <?php
@@ -99,11 +104,13 @@ function form_signin($erreur) {
         <input type="password" name="password" id="password" placeholder="Votre mot de passe"/>
         <img src="views/ressources/icons/info.png" alt="icone information" title="Saisissez le mot de passe associé à votre compte.">
       </p>
-      <p>
+      <p class="bouton_connexion">
         <input type="submit" value="Se connecter"/>
       </p>
-      <p>Pas encore inscrit ? Rejoignez-nous en cliquant <a href="index.php?cible=join-us">ici</a>.</p>
+      <p>Pas encore inscrit ? Rejoignez-nous en cliquant <a class="lien" href="index.php?cible=join-us">ici</a>.</p>
     </fieldset>
+    </article>
+  </section>
   </form>
   <?php
   $formulaire = ob_get_clean();
@@ -118,6 +125,8 @@ function form_subscribe_user() {
   ob_start();
   ?>
   <form method="post" action="index.php?cible=subscribe">
+    <section>
+    <article>
     <fieldset>
       <legend>Informations de connexion</legend>
         <p>
@@ -133,11 +142,13 @@ function form_subscribe_user() {
           <input type="password" name="conf_password" id="conf_password" placeholder="Confirmation du mot de passe" required/>
         </p>
     </fieldset>
+    </article>
 
+    <article>
     <fieldset>
       <legend>Informations personnelles</legend>
         <p>
-          Civilité<br/>
+          <label for="civilité"> Civilité <br/>
           <input type="radio" name="civilite" value="monsieur" id="monsieur"/>
           <label for="monsieur">Monsieur</label><br/>
           <input type="radio" name="civilite" value="madame" id="madame"/>
@@ -216,7 +227,9 @@ function form_subscribe_user() {
           </select>
         </p>
     </fieldset>
+    </article>
 
+    <article>
     <fieldset>
       <legend>Logement</legend>
         <p>
@@ -282,12 +295,14 @@ function form_subscribe_user() {
           <input type="text" name="superficie" id="superficie" placeholder="Superficie (m²)" required/>
         </p>
     </fieldset>
+    </article>
 
-    <p>
+    <p class="bouton">
       <input type="reset" value="Rafraichir">
       <input type="submit" value="S'inscrire"/>
     </p>
   </form>
+
   <?php
   $formulaire = ob_get_clean();
   return $formulaire;
@@ -356,6 +371,7 @@ function form_capteur_piece($nb_pièce) {
   <form method="post" action="index.php?cible=join-us_success">
     <input type="submit" value="Continuer sans sauvegarder" />
     </fieldset>
+    <section>
   </form>
 
   <?php
@@ -444,12 +460,14 @@ function current_date() {
 function my_home($user /*, $nb_notif*/) {
   ob_start();
   ?>
+  <section>
   <article>
     <p><strong>Mon domicile</strong></p>
     <p><strong><?php echo(current_date()); ?></strong></p>
 
 
   </article>
+  </section>
 
   <?php
   $home = ob_get_clean();
@@ -461,7 +479,7 @@ function my_information() {
   ob_start();
   $ligne = $data_user->fetch();
   ?>
-
+  <section>
   <article>
     <p><strong>Civilité : </strong><?php echo($ligne['civilite']); ?></p>
     <p><strong>Nom : </strong><?php echo($ligne['nom']); ?></p>
@@ -471,6 +489,7 @@ function my_information() {
     <p><strong>Ville : </strong><?php echo($ligne['ville']); ?></p>
     <p><strong>Pays : </strong><?php echo($ligne['pays']); ?></p>
   </article>
+  </section>
 
   <?php
   $info = ob_get_clean();
@@ -481,8 +500,31 @@ function my_information() {
 function content_products() {
   ob_start();
   ?>
-  <p> Voici la page principale "Produits" </p>
+  <section>
+  <article>
+  <p> <strong class="ehome">eHome</strong> met à votre disposition une large gamme d’équipements très divers vous permettant
+    d’enregistrer certaines valeurs afin d’adapter vos besoins par la suite en vous connectant à votre
+    compte personnel sur le site internet www.ehome.fr.
+    Quel est l’intérêt de tels dispositifs ?
+    L’habitation connectée offre à ses résidants un confort de haut niveau qui simplifie le quotidien de
+    chacun d’entre eux.</p>
 
+  <ul>Elle permet d’accommoder les nécessités individuelles :
+    <li class = "element_products"> Vous n’êtes pas souvent chez vous et voulez garder un oeil sur votre maison </li>
+    <li class = "element_products"> Vous êtes une personne à mobilité réduite </li>
+    <li class = "element_products"> Vous voulez simplement un système regroupant tous vos équipements électroniques afin de bénéficier d’un gain de temps et d’argent l’habitation connectée est faite pour vous. </li>
+  </ul>
+  <ul>Vous trouverez ici notre catalogue d’équipements domotiques, classés par type :
+    <li class = "element_products"> Capteurs : de luminosité, de température, de mouvement.</li>
+    <li class = "element_products"> Actionneurs : volets, portail, garage.</li>
+    <li class = "element_products"> Caméras : pour la surveillance</li>
+  </ul>
+
+  <p> Si vous ne trouvez pas le produit recherché ou si vous avez des questions, vous pouvez nous
+    contacter à l’adresse mail suivante : <a class="lien"> serviceclient@ehome.fr </a> ou par téléphone au <a class="lien"> 06.06.06.06.06 </a> </p>
+
+  </article>
+  </section>
   <?php
   $contenu = ob_get_clean();
   return $contenu;
@@ -492,7 +534,11 @@ function content_products() {
 function content_actuators() {
   ob_start();
   ?>
+  <section>
+  <article>
   <p>Voici la page principale "Actionneurs" </p>
+  </article>
+  </section>
   <?php
   $contenu = ob_get_clean();
   return $contenu;
@@ -502,7 +548,11 @@ function content_actuators() {
 function content_cameras() {
   ob_start();
   ?>
+  <section>
+  <article>
   <p>Voici la page principale "Caméras" </p>
+  </article>
+  </section>
   <?php
   $contenu = ob_get_clean();
   return $contenu;
@@ -512,7 +562,11 @@ function content_cameras() {
 function content_sensors() {
   ob_start();
   ?>
+  <section>
+  <article>
   <p>Voici la page principale "Capteurs" </p>
+  </article>
+  </section>
   <?php
   $contenu = ob_get_clean();
   return $contenu;
@@ -522,44 +576,46 @@ function content_sensors() {
 function content_about_us() {
   ob_start();
   ?>
-  <main>
-    <h1> <strong> Notre entreprise </strong> </h1>
+  <main class="main_about_us">
+    <section>
 
+    <article>
     <h2> Notre histoire </h2>
-
     <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
         ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
         humanitatis multiformibus officiis retentabant. Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
           ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
           humanitatis multiformibus officiis retentabant.</p>
-
     <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
         ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
         humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
         ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
         humanitatis multiformibus officiis retentabant.</p>
+    </article>
 
-    <ul>
-      <li> Créée en 2016 </li>
-      <li> 92% de satisfaction</li>
-      <li> Prix de l\'innovation 2016 </li>
+    <article>
+    <ul class = "content_about_us">
+      <li class = "element_about_us"> Créée en 2016 </li>
+      <li class = "element_about_us"> 92% de satisfaction</li>
+      <li class = "element_about_us"> Prix de l'innovation 2016 </li>
     </ul>
+    </article>
 
+    <article>
     <h2> Nos objectifs </h2>
-
     <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
         ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
         humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
           ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
           humanitatis multiformibus officiis retentabant.</p>
-
     <p> Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
         ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
         humanitatis multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma,
           ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi,
           humanitatis multiformibus officiis retentabant.</p>
+    </article>
 
-    <p><img src="views\ressources\images\maison_entreprise.jpeg" alt="Maison Entreprise"  title="Cliquez pour agrandir" /></p>
+  </section>
   </main>
   <?php
   $contenu = ob_get_clean();
