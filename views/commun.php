84b71d86e5a04800dbad98d45e0503d22ef008f8
FrenchPirate02 <aln.robic@gmail.com>
2017-06-02 10:07:52 +0200

    Footer placé en bas
    - Footer placé en bas sur la page join-us_type

--- views/commun.php
+++ views/commun.php
@@ -85,7 +85,7 @@ function menu_home() {
 
 
 // fonction permettant de choisir le type d'utilisateur
-function type_user() {
+function content_type_user() {
   ob_start();
   ?>
   <section>
@@ -176,6 +176,31 @@ function footer() {
   return $footer;
 }
 
+function footer2() {
+  ob_start();
+	?>
+  <div id="footer2">
+  	<ul class="footer2">
+      <ul>
+  		  <li class="footer_elements">Localisation</li>
+  		  <li class="footer_elements"><a href="https://www.google.fr/maps/place/ISEP/@48.8243885,2.2765791,16.25z/data=!4m5!3m4!1s0x47e670797ea4730d:0xe0d3eb2ad501cb27!8m2!3d48.824529!4d2.2798536" target="_blank"><img class="map" src="views\ressources\images\map_isep.png" alt="Map rue de Vanves"  title="Cliquez ici pour afficher dans Google Maps" /></a></li>
+      </ul>
+
+      <ul>
+  		  <li class="footer_elements"><p><a class="lien" href="index.php?cible=legal_information">Mentions légales</a></li>
+      </ul>
+
+      <ul>
+  		  <li class="footer_elements">Contact</li>
+  		  <li class="footer_elements">+33 1 23 45 67 89</li>
+  		  <li class="footer_elements"> accueil@ehome.com</li>
+      </ul>
+  	</ul>
+  </div>
+	<?php
+  $footer = ob_get_clean();
+  return $footer;
+}
 
 function footer_s() {
   ob_start();
