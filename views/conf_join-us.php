bf416345ab3569e49a8575d3ffcad86f228cf982
FrenchPirate02 <aln.robic@gmail.com>
2017-06-27 12:08:32 +0200

    ajout photo partout
    - Photos partout

--- views/conf_join-us.php
+++ views/conf_join-us.php
@@ -6,7 +6,7 @@ $titre = 'Confirmation de l\'inscription';
 
 $menu = menu();
 
-$contenu = '<section><h2>Vous êtes dorénavant inscrit</h2>';
+$contenu = '<section class="conf_join-us"><h2>Vous êtes dorénavant inscrit</h2>';
 $contenu .= '<p>Veuillez vous <a class="lien" href="index.php?cible=signin">connecter</a> pour continuer</p></section>';
 
 $footer = footer();
